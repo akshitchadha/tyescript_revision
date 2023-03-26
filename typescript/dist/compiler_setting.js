eaaa27d700ca31e494ec8cb4a4a4a56dc0d52f28
WOLF_KING <achadha1995@gmail.com>
2023-03-26 16:40:16 +0530

    Delete typescript directory

--- typescript/dist/compiler_setting.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// to compile the file use command tsc  abc.ts
-const norma = {
-    breed: "abc",
-    eggsPerWeek: 2,
-    name: "xyz",
-    age: 7
-};
--- typescript/dist/product.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const printProductInformation = (product) => {
-    console.log(`${product.name}  - ${product.price}  `);
-};
